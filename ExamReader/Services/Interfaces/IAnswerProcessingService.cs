00fe303457adb865a25905d06aa6b7b01429a36e
Mehmet Can Diri <mehmetcandiri@gmail.com>
2024-04-05 15:40:29 +0300

    first commit

---
+++ ExamReader/Services/Interfaces/IAnswerProcessingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ExamReader.Models;
+
+namespace ExamReader.Services.Interfaces
+{
+	public interface IAnswerProcessingService
+	{
+        Task<Dictionary<int, string>> ExtractAnswersFromImageAsync(Stream imageStream);
+        Task<ProcessingResult> ProcessUploadedFileAsync(IFormFile fileUpload, string[] answerKey);
+
+    }
+}
+
